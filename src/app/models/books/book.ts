1e6af0f03cc135ae4903e72d854bd63becf2065e
harilearning1989 <59171886+harilearning1989@users.noreply.github.com>
2023-04-13 22:23:12 +0530

    fix

--- src/app/models/books/book.ts
+++ src/app/models/books/book.ts
@@ -1,21 +1,15 @@
 export class Book {
 
   id: number = 0;
-  subject: string = "";
   bookName: string = "";
+  subject: string = "";
   isbn: string = "";
-  bookQty: number = 0;
-  authors: string = "";
-  pageCount: number = 0;
   price: number = 0;
+  bookQty: number = 0;
   availBooks: number = 0;
+  authors: string = "";
   publishedDate: string = "";
-  thumbnailUrl: string = "";
   shortDescription: string = "";
   longDescription: string = "";
-  status: string = "";
-  categories: string = "";
-  createdDate: string = "";
-  updatedDate: string = "";
 
 }
